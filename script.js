8d24549f650722c0a3456ea6ccd8fdf9cc14d63c
jmerges bootcamp <jamesmerges1@gmail.com>
2020-12-18 02:14:07 -0800

    Added character sets for the password

--- script.js
+++ script.js
@@ -12,3 +12,20 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Original code starts here:
+//___________________________
+
+// generate lower case alphabet array
+var lowerCaseAlphabet = [
+  'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'
+];
+var upperCaseAlphabet = [
+  'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
+];
+var digits = [
+  '0','1','2','3','4','5','6','7','8','9'
+];
+var specialCharacters = [
+  ' ','!','"','#','$','%','&',"'",'(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[','\\',']','^','_','`','{','|','}','~'
+];
